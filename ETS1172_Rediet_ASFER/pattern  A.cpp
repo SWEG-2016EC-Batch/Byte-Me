040ea8b938b2de9e496c46dc25b1a2a0159812e7
Rediet ASFER <redietasfer9@gmail.com>
2024-12-15 19:41:33 +0300

    pattern  A.cpp

--- ETS1172_Rediet_ASFER/pattern  A.cpp
+++ ETS1172_Rediet_ASFER/pattern  A.cpp
@@ -35,7 +35,7 @@ for(int i = 1; i<= 5; i++){
     for(int j = i; j<=5; j++){
         cout << " ";
     }
-    for(int j = 1; j <= i; j++){
+    for(int j = i; j >= i; j--){
         cout  << j ;
     }
     cout << endl;
